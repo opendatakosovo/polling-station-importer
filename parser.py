d060b58393af84b4b4d6b53950a104ab15b845a8
legeorges <georges.labreche@gmail.com>
2014-06-16 08:46:15 -0700

    added values without diacritical marks in mongo document. This is so that we can search without diacritic marks.

--- parser.py
+++ parser.py
@@ -1,4 +1,6 @@
 import csv
+import unicodedata
+
 from pymongo import MongoClient
 
 csv_filename = 'polling-stations-general-elections-2014.csv'
@@ -13,58 +15,87 @@ collection = db.pollingstations
 # Clear data
 db.pollingstations.remove({})
 
-with open(csv_filename, 'rb') as csvfile:
-	reader = csv.reader(csvfile)
-	
-	# Skip the header
-	next(reader, None)
-	
-	# Iterate through the rows, retrieve desired values.
-	for row in reader:
-		polling_station_id = row[3]
-		commune_id = row[1]
+def remove_diacritic(str):
+    '''
+    Accept a unicode string, and return a normal string (bytes in Python 3)
+    without any diacritical marks.
+    '''
+    return unicodedata.normalize('NFKD', str.decode('utf-8')).encode('ASCII', 'ignore')
+
+def parse_csv():
+	'''
+	Reads the election polling station CSV file.
+	Creates Mongo document for each polling station.
+	Stores document.
+	'''
+	with open(csv_filename, 'rb') as csvfile:
+		reader = csv.reader(csvfile)
 		
-		if('/' in row[2]):
-			commune_names = row[2].split('/')
-			commune_name_sq = commune_names[0]
-			commune_name_sr = commune_names[1]
-		else:
-			commune_name_sq = row[2]
-			commune_name_sr = row[2]
-			print 'Missing commune name translation for polling station {0}: {1}'.format(polling_station_id, commune_name_sq)
+		# Skip the header
+		next(reader, None)
 		
-		if('/' in row[4]):
-			cities = row[4].split('/')
-			city_sq = cities[0]
-			city_sr = cities[1]
-		else:
-			city_sq = row[4]
-			city_sr = row[4]
-			print 'Missing city name translation for polling station {0}: {1}'.format(polling_station_id, city_sq)
-		
-		polling_station_name_sq = row[5]
-		polling_station_name_sr = row[6]
+		# Iterate through the rows, retrieve desired values.
+		for row in reader:
+			polling_station_id = row[3]
+			commune_id = row[1]
 			
-		polling_station = {
-			'_id': polling_station_id,
-			'name': {
-				'sq': polling_station_name_sq.strip(),
-				'sr': polling_station_name_sr.strip()
-			},
-			'city': {
-				'sq': city_sq.strip(),
-				'sr': city_sr.strip()
-			},
-			'commune': {
-				'id': commune_id,
-				'sq': commune_name_sq.strip(),
-				'sr': commune_name_sr.strip()
-			},
-			'coordinates': {
-				'lon': 0,
-				'lat': 0
+			if('/' in row[2]):
+				commune_names = row[2].split('/')
+				commune_name_sq = commune_names[0]
+				commune_name_sr = commune_names[1]
+			else:
+				commune_name_sq = row[2]
+				commune_name_sr = row[2]
+				print 'Missing commune name translation for polling station {0}: {1}'.format(polling_station_id, commune_name_sq)
+			
+			if('/' in row[4]):
+				cities = row[4].split('/')
+				city_sq = cities[0]
+				city_sr = cities[1]
+			else:
+				city_sq = row[4]
+				city_sr = row[4]
+				print 'Missing city name translation for polling station {0}: {1}'.format(polling_station_id, city_sq)
+			
+			polling_station_name_sq = row[5]
+			polling_station_name_sr = row[6]
+				
+			polling_station = {
+				'_id': polling_station_id,
+				'name': {
+					'sq': polling_station_name_sq.strip(),
+					'sr': polling_station_name_sr.strip()
+				},
+				'city': {
+					'sq': city_sq.strip(),
+					'sr': city_sr.strip()
+				},
+				'commune': {
+					'id': commune_id,
+					'sq': commune_name_sq.strip(),
+					'sr': commune_name_sr.strip()
+				},
+				'coordinates': {
+					'lon': 0,
+					'lat': 0
+				},
+				'ascii': {
+					'name': {
+						'sq': remove_diacritic(polling_station_name_sq.strip()),
+						'sr': remove_diacritic(polling_station_name_sr.strip())
+					},
+					'city': {
+						'sq': remove_diacritic(city_sq.strip()),
+						'sr': remove_diacritic(city_sr.strip())
+					},
+					'commune': {
+						'sq': remove_diacritic(commune_name_sq.strip()),
+						'sr': remove_diacritic(commune_name_sr.strip())
+					}
+				}
 			}
-		}
+			
+			# Store polling station as document in mongo.
+			collection.insert(polling_station)
 		
-		# Store polling station as document in mongo.
-		collection.insert(polling_station)
+parse_csv()
